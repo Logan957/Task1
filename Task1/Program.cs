45f4b65f958298d9ad5cf14bdab59b317e7174c0
Logan957 <roman.zaidulin2015@gmail.com>
2020-10-03 12:56:49 +0300

    First commit

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value for how many decimals to display - Limit is 14");
+            int value = System.Convert.ToInt32(Console.ReadLine());
+            value = value >14 ? 14 : value;
+            const double pi = System.Math.PI;
+            Output(value, pi);
+
+        }
+        static void Output(int value, double pi ) 
+        {
+            Console.WriteLine(pi.ToString("F" + value));
+        }
+    }
+}
